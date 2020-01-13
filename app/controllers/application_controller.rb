6cc19c0be98be40eafe6d88ab6ff2857a3b2173f
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-01-13 12:56:21 +0900

    Add view of notes#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permited_parameters, if: :device_controller?
+  before_action :configure_permited_parameters, if: :devise_controller?
   protected
   def configure_permited_parameters
-    device_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
