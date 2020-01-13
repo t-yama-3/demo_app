7c4b5d6e1c70174ff94941492d80b0762004e025
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-01-13 13:50:00 +0900

    Update notes_controller.rb

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,12 @@
 class NotesController < ApplicationController
   def index
+    @note = Note.new
+  end
+
+  def create
+    # binding.pry
+    @note = Note.new(body: params[:note][:body], user_id: current_user.id)
+    @note.save
+    redirect_to root_path
   end
 end
