605bc083e3ec814a82d4fb989229059467eaa41e
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-01-13 11:53:52 +0900

    Add name column to user table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permited_parameters, if: :device_controller?
+  protected
+  def configure_permited_parameters
+    device_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
