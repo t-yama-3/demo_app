e217453ee8d99c7ddbb5a443e902339cf7e026e7
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-04-17 22:16:04 +0900

    add payjp links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'purchase/index'
-  get 'purchase/done'
+  # get 'purchase/index'
+  # get 'purchase/done'
   # get 'card/new'
   # get 'card/show'
   devise_for :users
